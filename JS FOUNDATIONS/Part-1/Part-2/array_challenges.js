3de3cc402029a654a78193fdd9d67fbc5aa81af7
AmritParashar1 <145751993+AmritParashar1@users.noreply.github.com>
2024-10-05 19:45:42 +0530

    array.js and conditions.js

---
+++ JS FOUNDATIONS/Part-1/Part-2/array_challenges.js
@@ -0,0 +1,94 @@
+//Challenge 1 : declare an array and then extract the first element and assign it to the variable.
+
+let teaFlowers = ["green tea","black tea","oolong tea"];
+
+let teafl = new Array("green tea","black tea","oolong tea");  //another way of initializing an array
+
+// console.log(teaFlowers[0]);
+
+firstTea = teaFlowers[0];
+
+// console.log(firstTea);
+
+//Challenge 2: declare an array and extract the third element and assign it to the variable
+
+let cities = ["London","Tokyo","Paris","New York"];
+
+// console.log(cities[2]);
+
+favoriteCity = cities[2];
+
+// console.log(favoriteCity);
+
+//Challenge 3 : declare an array named teaTypes and change the second element of an array to "jasmine tea"
+
+let teaTypes = ["herbal tea","white tea","masala chai"];
+
+// console.log("Third element before : "+teaTypes[2]);
+
+teaTypes[2] = "jasmine tea";
+
+// console.log("Third element after : "+teaTypes[2]);
+
+//Challenge 4: Declare an array named citiesVisited containing "Mumbai" and "Sydney"
+
+let citiesVisited = ["Mumbai","Sydney"];
+// citiesVisited.push("Berlin");
+// citiesVisited[2] = "Berlin";
+// citiesVisited[citiesVisited.length] = "Berlin"
+
+// console.log(citiesVisited);
+
+//Challenge 5: Pop one element from the array and assign in to the variable
+
+let teaOrders = ["chai","iced tea","matcha","earl grey"];
+let n = teaOrders.length;
+lastOrder = teaOrders[n-1];
+
+teaOrders.pop();
+
+// console.log(teaOrders);
+// console.log(lastOrder);
+
+//Challenge 6: create a soft copy of an array
+
+let popularTeas = ["green Tea","oolong tea","chai"];
+
+let softCopyTeas = popularTeas;
+
+popularTeas.pop();
+
+//In this what is happening that i am deleting something from the main array then it is getting deleted from the copy also.
+
+// console.log(softCopyTeas);
+// console.log(popularTeas);
+
+//Challenge 7: create a hard copy of an array
+
+let topCities = ["Berlin","Singapore","New York"];
+let hardCopyCities = [...topCities];
+topCities.pop();
+
+// console.log(hardCopyCities);
+// console.log(topCities);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JS FOUNDATIONS/Part-1/Part-2/conditions.js
@@ -0,0 +1,86 @@
+//challenge 1 : check if a number is greater than another number
+
+let num1 = 5;
+let num2 = 7;
+
+if(num1>num2)
+{
+    // console.log("The greater number is :"+ num1);
+    
+}
+
+else 
+{
+    // console.log("The greater number is :"+num2);
+    
+}
+
+
+//Challenge 2 : Check if a string is equal to another string 
+
+let username = "chai";
+let anotherusername = "chai";
+
+if(username == anotherusername)
+{
+    // console.log("Pick another username");
+    
+}
+
+else
+{
+    // console.log("You can pick this username");
+    
+}
+
+//Challenge 3: if a variable is a number or not
+
+let score = 44;
+
+if(typeof score === "number")
+{
+    // console.log("score is a number");
+    
+}
+else
+{
+    // console.log("score is not a number");
+    
+}
+
+//Challenge 4: checking if a boolean value is true or not
+
+let isTeaReady = false;
+
+if(isTeaReady)
+{
+    // console.log("Tea is ready");
+    
+}
+
+else 
+{
+    // console.log("Tea is not ready");
+    
+}
+
+
+//Challenge 5 : Check if array is empty or not
+
+let items = [];
+
+console.log(items.length);
+
+if(items.length == 0)
+{
+    console.log("array is empty");
+    
+}
+else
+{
+    console.log("array is not empty");
+    
+}
+
+
+
