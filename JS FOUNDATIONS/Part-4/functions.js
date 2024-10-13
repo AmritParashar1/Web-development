b4539dc4709852e95ef4d62b192fc3279fea89f0
AmritParashar1 <145751993+AmritParashar1@users.noreply.github.com>
2024-10-13 13:06:11 +0530

    Update functions.js

--- JS FOUNDATIONS/Part-4/functions.js
+++ JS FOUNDATIONS/Part-4/functions.js
@@ -27,16 +27,60 @@ let orderConfirmation = orderTea("Chai");
 
 //Challenge 3: Write an arrow function 
 
-// const calculateTotal = (price,quantity) => {
-//     return price * quantity;
 
-// }
+// normal function :- function greet() {}
+// arrow function :- const greet = () => {}
 
-// OR
+//one way 
+
+// const calculateTotal = (price , quantity) => {
+//     return price*quantity;
+// }  
+
+// let totalCost = calculateTotal(499*100); 
+
+//2nd way : where you will get rid of curly braces and remove the return part and apprantly the code should be in one line.
 
 const calculateTotal = (price,quantity) => price*quantity;
 
-let totalCost = calculateTotal(499*100);
+let totalCost = calculateTotal(399,13000);
+
+// console.log(totalCost);
+
+//Challenge 4 : Higher order function or First class function
+
+function makeTea(typeOfTea){
+    return `maketea : ${typeOfTea}`;
+}
+function processTeaOrder(teaFunction)
+{
+    return teaFunction('earl grey')
+}
+
+let order = processTeaOrder(makeTea);
+// console.log(order);
+
+//Challenge 5: 
+
+function createTeaMaker(name)
+{
+    return function(teaType)
+    {
+        return `Making ${teaType}`;
+    }
+}
+
+let teaMaker = createTeaMaker("amrit");
+let result = teaMaker("green tea");
+console.log(result);
+
+//there is a feature in javaScript which is known as closure which ensures that the variables of the outer function can be accessed by the inner functions. 
+
+
+
+
+
+
 
 
 
